f48ec530dda0a70f94d10cb9349673765aa53600
Katherin Anampa <sv46114380@idat.edu.pe>
2024-05-13 22:49:30 -0500

    javascript_2

---
+++ app.js
@@ -0,0 +1,83 @@
+//const message = prompt('Ingresa tu nombre');
+
+
+//const question = prompt()
+
+//const fullName = 'Katherin Anampa';
+
+//console.log(fullName.length);
+
+
+//console.log('LIMA'.toLocaleLowerCase());
+
+//if(question.toLocaleLowerCase()=== 'lima'){
+   // console.log('ganastes');
+//}
+
+//const name= 'kate';
+//const lastName = 'anampa';
+
+//concatenacion
+
+//console.log('Bienvenido' + ' ' + name + ' ' + lastName + ' al bootcamp');
+
+//template string 
+
+//console.log('Bienvenido ${name} ${lastName} al bootcamp');
+
+
+//Modulo
+//value = 20 % 3;
+//value = (20 + 10 + 30) * 0.5;
+
+//console.log(value);
+
+const banana = 30.5;
+const orange = 10;
+const apple  = 15.5;
+
+let total, descuento, totalF;
+// Mnadar a consola el pago total
+// Te dicen que te daran un descuento del pago total del 30%
+// Mandar a consola 3 valores
+
+// 1. Costo total antes del descuento
+// 2. Valor del descuento
+// 3. Costo final aplicado el descuento
+
+
+// usar concatenacion
+
+total = banana + orange + apple ;
+descuento = total * 0.30;
+totalF = total - descuento;
+console.log('El total es:' + total);
+console.log('El descuento es:' + descuento);
+console.log('Costo final es:' + totalF);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,29 @@
+# JAVASCRIPT
+
+## METODOS STRING
+
+Proporciona métodos para el tratamiento de las cadenas de caracteres: acceso a caracteres individuales, buscar y extraer una subcadena, copiar cadenas, convertir cadenas a mayúsculas o minúsculas, etc.
+
+```
+const frase = "Hola"
+console.log('Hola')
+
+```
+
+
+### TOUPPERCASE
+
+Simplemente devuelve la cadena en mayúsculas.
+
+```
+cadena.toUpperCase()
+
+```
+
+
+## 🛠 Skills
+HTML, JAVASCRIPT, CSS...
+
+## Authors
+
+- [@Katherin-Anampa](https://www.github.com/kate-anampa)
